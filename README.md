a8588258b41fa49bf8bb87ee497ed8fba25c2172
Alex Gavryushkin <alex@gavruskin.com>
2017-03-07 10:32:28 +0100

    data preprocessing in one file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Pipeline
 
 - The data file is assumed to be in the format as of Feb 21 and in the root folder.
-- To generate the data file required for the model, run:
-	- `python data_preprocess_add_parameter_columns.py`
-	- `python data_preprocess_add_parameters.py`
-- Run the analysis: `python taylor_lin_fit.py`
+- To generate the data file required for the model, run `python data_preprocess.py`
+- To run the analysis: `python taylor_lin_fit.py`
 
--- data_preprocess.py
+++ data_preprocess.py
@@ -1,8 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("fitness_summary_all_replicates_parameter_columns.csv", sep="\t")
+data = pd.read_csv("fitness_summary_all_replicates.csv")
+for i in range(data.shape[0]):
+        for j in range(16, 48):
+                data.set_value(i, j, 0)
+data.rename(columns={16: "a", 17: "a1", 18: "a2", 19: "a3", 20: "a4", 21: "a5",
+        22: "b12", 23: "b13", 24: "b14", 25: "b15", 26: "b23", 27: "b24",
+        28: "b25", 29: "b34", 30: "b35", 31: "b45", 32: "c123", 33: "c124",
+        34: "c125", 35: "c134", 36: "c135", 37: "c145", 38: "c234", 39: "c235",
+        40: "c245", 41: "c345", 42: "d1234", 43: "d1235", 44: "d1245",
+        45: "d1345", 46: "d2345", 47: "e12345"}, inplace=True)
+
 for index, row in data.iterrows():
 	species = row["LP"] + row["LB"] + row["AP"] + row["AT"] + row["AO"]
 	if species == "YNNNN":
@@ -280,6 +289,6 @@ for index, row in data.iterrows():
 		data.set_value(index, "e12345", 1)
 	if species == "NNNNN":
 		data.set_value(index, "a", 1)
-del data["Unnamed: 0"]
+#del data["Unnamed: 0"]
 data.to_csv("fitness_summary_all_replicates_parameters.csv", sep="\t")
 
